822f2b9677c1aa58f636f9c4ab817d1f6d75abc1
user7 <rupalime@cybage.com>
2015-10-27 19:14:29 +0530

    circle.yml file changed8

--- test/test2.js
+++ test/test2.js
@@ -1,6 +1,6 @@
 var mocha=require('mocha');
 var chai=require('chai');
-var server=require('..\\server.js');
+var server=require('..\server.js');
 var status =require('http-status');
 var assert = chai.assert;
 var users = require('..\\routes\\user.js');
