363ffdc48d7b33832e5f4ef07b79215c7edc1f33
Rupali Mehta <rupalimehta05@gmail.com>
2015-10-29 18:31:30 +0530

    Update test2.js

--- test/test2.js
+++ test/test2.js
@@ -3,22 +3,12 @@ var chai=require('chai');
 var server=require('..\server.js');
 var status =require('http-status');
 var assert = chai.assert;
-var users = require('..\\routes\\user.js');
+var users = require('..\routes\user.js');
 var superagent = require('superagent');
 var bodyParser = require('body-parser')
 
 
-  describe('//users', function(){
-  /* {
-  var app;
-    before(function() {
-    app = server(3000);
-  });
- 
-  after(function() {
-    app.close();
-  });
-  */
+  describe('/users', function(){
 		 
   it('returns username if name param is a valid user', function() {
    // users.list = ['test'];
