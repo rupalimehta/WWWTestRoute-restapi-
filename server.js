13be97485c96ecd82921bff649147c65471a73a0
Rupali Mehta <rupalimehta05@gmail.com>
2015-11-02 19:06:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var express = require('express')
  var app = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
+  app.set('port', process.env.PORT || 3040);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.favicon());
@@ -25,7 +25,7 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 app.get('/users', user.list);
-/*
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
-});*/
+});
